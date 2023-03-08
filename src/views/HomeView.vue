f1a248f419ccba2f6614557e1cb20b0ccf32a574
rdpnr_wangguodong <you@example.com>
2023-03-08 14:09:32 +0800

    提交excel的上传和导出功能

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import UploadExcel from "@/components/UploadExcel.vue";
+import ExportExcel from "@/components/ExportExcel/index.vue";
+import {ref} from "vue";
+
+const excelData = ref();
+const onSuccess = (data:any)=>{
+  console.log(data, 'data')
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <UploadExcel :on-success="onSuccess" />
+    <ExportExcel></ExportExcel>
   </main>
 </template>
